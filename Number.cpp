8dd584d8ab7fb31bf72d211f34b61e1ea0bed502
Yaroslav <yaroslav.bondarenko@frtk.ru>
2016-10-23 03:25:46 +0300

    Addition and subtraction complete
    1. Basic arithmetic operations are complete;
    2. Comparisons implemented;
    3. Basic code documentation;
    4. Tested operations with various lengths, signs, border values.

--- Number.cpp
+++ Number.cpp
@@ -16,67 +16,235 @@ Number::Number(const Number &cp)
         const_cast<Number&>(cp)._num = NULL;
     }
     else
-    {
-        this->_num = new TWORD[cp._size];
-        memcpy(this->_num, cp._num, cp._size * sizeof(TWORD));
-    }
-    this->_size = cp._size;
+        this->_num = new std::vector<TWORD>(*cp._num);
+
+    this->_sign = cp._sign;
 }
 
 Number Number::operator+(Number &add) {
-    USHORT size = MAX(_size, add._size) + (USHORT)1;
-    TWORD *ptr = new TWORD[size];
-    TWORD *extra = NULL;
-    TDWORD tmp = 0;
-    USHORT i;
+    std::vector<TWORD> *ptr;
+    bool sign = _sign;
 
-    if (_size > add._size)
-        extra = _num;
-    else if (add._size > _size && i < add._size)
-        extra = add._num;
+    // Signs are equal -> addition
+    if (_sign && add._sign || !_sign && !add._sign)
+    {
+        ptr = __add(*_num, *add._num);
+        sign = _sign;
+    }
+    else
+    {
+        int res = modcmp(*this, add);
 
-    for (i = 0; i < size - 1; ++i)
-        if (i < MIN(_size, add._size))
+        // Trivial result
+        if (res == 0)
         {
-            tmp = (TDWORD)this->_num[i] + add._num[i] + HIWORD(tmp);
-            ptr[i] = LOWORD(tmp);
+            ptr = new std::vector<TWORD>;
+            ptr->push_back(0);
+            return Number(ptr, true, true);
+        }
+        else if (res == -1)
+        {
+            sign = add._sign;
+            ptr = __sub(*add._num, *_num);
         }
         else
-        {
-            tmp = (TDWORD) extra[i] + HIWORD(tmp);
-            ptr[i] = LOWORD(tmp);
-        }
+            ptr = __sub(*_num, *add._num);
+    }
 
-    if (HIWORD(tmp))
-        ptr[size - 1] = HIWORD(tmp);
+    return Number(ptr, sign, true);
+}
+
+Number Number::operator-(Number &sub) {
+    std::vector<TWORD> *ptr;
+    bool sign = _sign;
+
+    // Sign combination is equal to addition
+    if (_sign && !sub._sign || !_sign && sub._sign)
+    {
+        ptr = __add(*_num, *sub._num);
+        sign = _sign;
+    }
     else
-        ptr = (TWORD *) realloc(ptr, --size);
+    {
+        int res = modcmp(*this, sub);
 
-    return Number(ptr, size, true);
+        // Trivial result
+        if (res == 0)
+        {
+            ptr = new std::vector<TWORD>;
+            ptr->push_back(0);
+            return Number(ptr, true, true);
+        }
+        else if (res == -1)
+        {
+            sign = !sub._sign;
+            ptr = __sub(*sub._num, *_num);
+        }
+        else
+            ptr = __sub(*_num, *sub._num);
+    }
+
+    return Number(ptr, sign, true);
 }
 
 Number& Number::operator=(const Number &assign)
 {
+    // Free contents
     if (!this->_num)
         delete _num;
 
+    // Retrieving
     if (assign._tmp)
     {
         this->_num = assign._num;
         const_cast<Number&>(assign)._num = NULL;
     }
+    // Copying
     else
     {
-        this->_num = new TWORD[assign._size];
-        memcpy(this->_num, assign._num, assign._size * sizeof(TWORD));
+        this->_num = new std::vector<TWORD>;
+        *_num = *assign._num;
     }
-    this->_size = assign._size;
+    this->_sign = assign._sign;
 
     return *this;
 }
 
-USHORT Number::getContent(TWORD **dest)
+std::vector<TWORD>* Number::__add(std::vector<TWORD> &a, std::vector<TWORD> &b)
 {
-    *dest = this->_num;
-    return this->_size;
+    std::vector<TWORD> *ret = new std::vector<TWORD>;
+    std::vector<TWORD> &extra = (a.size() > b.size()) ? a : b;
+    TDWORD tmp = 0;
+
+    for (unsigned long i = 0; i < MAX(a.size(), b.size()); ++i)
+    {
+        if (i < MIN(a.size(), b.size()))
+            tmp = (TDWORD) a[i] + b[i] + HIWORD(tmp);
+        else
+            tmp = (TDWORD) extra[i] + HIWORD(tmp);
+
+        ret->push_back(LOWORD(tmp));
+    }
+    if (HIWORD(tmp))
+        ret->push_back(HIWORD(tmp));
+
+    return ret;
+}
+
+std::vector<TWORD>* Number::__sub(std::vector<TWORD> &a, std::vector<TWORD> &b)
+{
+    std::vector<TWORD> *ret = new std::vector<TWORD>;
+    std::vector<TWORD> &extra = (a.size() > b.size()) ? a : b;
+    unsigned short cf = 0;
+    unsigned long count = 0;
+
+    for (unsigned long i = 0; i < MAX(a.size(), b.size()); ++i)
+        if (i < MIN(a.size(), b.size()))
+            if (a[i] >= b[i] + (TDWORD)cf)
+            {
+                ret->push_back(a[i] - b[i] - cf);
+                cf = 0;
+            }
+            else
+            {
+                ret->push_back(a[i] + (TDWORD)(1 << (sizeof(TWORD) * 8)) - b[i] - cf);
+                cf = 1;
+            }
+        else
+            if (extra[i] >= cf)
+            {
+                ret->push_back(extra[i] - cf);
+                cf = 0;
+            }
+            else
+            {
+                ret->push_back(extra[i] + (TDWORD)(1 << (sizeof(TWORD) * 8)) - cf);
+                cf = 1;
+            }
+
+    // Removal of leading zeros
+    for (unsigned long i = ret->size(); i > 0; --i)
+        if (!ret->at(i - 1))
+            ++count;
+        else
+            i = 1;
+    if (count)
+        ret->resize(ret->size() - count);
+
+    return ret;
+}
+
+bool Number::operator<=(Number &arg)
+{
+    return !operator>(arg);
+}
+
+bool Number::operator>=(Number &arg)
+{
+    return !operator<(arg);
+}
+
+bool Number::operator>(Number &arg)
+{
+    if (_sign && !arg._sign)
+        return true;
+    if (!_sign && arg._sign)
+        return false;
+    return modcmp(*this, arg) == 1 && _sign;
+}
+
+bool Number::operator<(Number &arg)
+{
+    if (_sign && !arg._sign)
+        return false;
+    if (!_sign && arg._sign)
+        return true;
+    return modcmp(*this, arg) == -1 && _sign;
+}
+
+bool Number::operator==(Number &arg)
+{
+    if (_num->size() != arg._num->size() || _sign != arg._sign)
+        return false;
+    else
+        for (unsigned long i = 0; i < _num->size(); i++)
+            if (_num[i] != arg._num[i])
+                return false;
+    return true;
+}
+
+void Number::negate(void)
+{
+    _sign = !_sign;
+}
+
+int Number::modcmp(Number &a, Number &b)
+{
+    if (a._num->size() != b._num->size())
+        return (a._num->size() > b._num->size()) ? 1 : -1;
+    for (unsigned long i = a._num->size(); i > 0; i--)
+        if (a._num->at(i - 1) > b._num->at(i - 1))
+            return 1;
+        else if (a._num->at(i - 1) < b._num->at(i - 1))
+            return -1;
+    return 0;
+}
+
+std::ostream& LongNumber::operator<<(std::ostream &out, Number& src)
+{
+    out << ((src._sign) ? "0x" : "-0x");
+    for (unsigned long i = src._num->size(); i >0; --i)
+        out << std::setfill('0') << std::setw(sizeof(TWORD) * 2)
+            << std::hex << std::uppercase << (*src._num)[i - 1];
+    return out;
+}
+
+unsigned long Number::getSize(void)
+{
+    return _num->size();
+}
+
+bool Number::getSign(void)
+{
+    return _sign;
 }
--- Number.h
+++ Number.h
@@ -2,9 +2,11 @@
 #define __NUMBER__
 
 #include <cstddef>
-#include <pth.h>
 #include <cstring>
 #include <malloc.h>
+#include <iostream>
+#include <iomanip>
+#include <vector>
 
 #define MAX(a,b) ((a>b)?a:b)
 #define MIN(a,b) ((a>b)?b:a)
@@ -13,25 +15,105 @@
 
 namespace LongNumber
 {
+    /// Main type of basic unit
     typedef unsigned int TWORD;
+    /// Twice as large as TWORD
     typedef unsigned long TDWORD;
-    typedef unsigned short USHORT;
 
+    ///Basic class for long calculation
     class Number
     {
     protected:
-        TWORD *_num;
-        USHORT _size;
-        bool _tmp = false;
-        Number(TWORD *number, USHORT size, bool tmp): _num(number), _size(size), _tmp(tmp){};
+        std::vector<TWORD> *_num;   ///< Data storage
+        bool _tmp = false;          ///< Operator-specific
+                                    ///< Used explicitly in operators for performance optimizing.
+                                    ///< Do not move to public fields.
+                                    ///<
+                                    ///<  true - contents should be retrieved instead of copying;
+                                    ///< false - explicit copy is required
+
+        bool _sign;                 ///< true  - positive, false - negative
+
+        /** Operator-specific
+         *  Used in operators to make use of _tmp flag
+         *
+         * @param number - vector of basic units
+         * @param sign   - sets the _sign
+         * @param tmp    - could the contents be retrieved or should be copied
+         */
+        Number(std::vector<TWORD> *number, bool sign, bool tmp):
+                _num(number), _tmp(tmp), _sign(sign){};
+
     public:
-        Number(TWORD *number, USHORT size): _num(number), _size(size){};
+        /** Commom constructor
+         *
+         * @param number - vector of basic units
+         * @param sign   - true = positive, false = negative (0 is positive)
+         */
+        Number(std::vector<TWORD> *number, bool sign): _num(number), _sign(sign){};
+
+        /** Copy constructor, mainly used for assignment (=)
+         *
+         * @param cp - target Number to copy (temporary ones are emptied)
+         */
         Number(const Number& cp);
+
+        /**
+         * @return          temporary Number object (contents to be retrieved)
+         */
         Number operator+ (Number &add);
+
+        /**
+         * @return          temporary Number object (contents to be retrieved)
+        */
+        Number operator- (Number &sub);
+        /** Retrieves contents from temporary objects; otherwise copies them
+         *
+         * @return          -   reference to itself for chain assignments
+         */
         Number& operator= (const Number &assign);
-        USHORT getContent(TWORD** dest);
+
+        bool operator == (Number &arg);
+        bool operator > (Number &arg);
+        bool operator < (Number &arg);
+        bool operator >= (Number &arg);
+        bool operator <= (Number &arg);
+
+        /// Reverses the sign
+        void negate(void);
+        unsigned long getSize(void);
+        bool getSign(void);
+
+        /** Compares absolute values
+         *
+         * @return      a = b   -   0
+         *              a > b   -   1
+         *              a < b   -   -1
+         */
+        static int modcmp(Number &a, Number &b);
+
+        friend std::ostream& operator<< (std::ostream& out, Number& src);
         ~Number();
+
+    protected:
+        /** Addition of absolute values
+         *
+         * @return  result vector of TWORD without leading zeros
+         */
+        static std::vector<TWORD>* __add(std::vector<TWORD> &a, std::vector<TWORD> &b);
+
+        /** Subtraction of absolute values
+         *
+         * @return  result vector of TWORD without leading zeros
+         */
+        static std::vector<TWORD>* __sub(std::vector<TWORD> &a, std::vector<TWORD> &b);
     };
+
+    /** Hexadecimal output to standard streams (cout, string)
+     *
+     * @return  reference to itself for chain assignments
+     */
+    std::ostream& operator<< (std::ostream& out, Number& src);
 }
 
 #endif
